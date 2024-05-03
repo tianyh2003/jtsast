cd1183a7664039dc2f617f9aa12080081e7d8039
tyh <1798876669@qq.com>
2024-05-04 00:51:59 +0800

    modify input/output dir check

--- src/InputUnit.js
+++ src/InputUnit.js
@@ -7,12 +7,12 @@ class InputUnit {
     constructor(_in_addr, _out_addr) {
         this.in_addr = _in_addr; 
         this.out_addr = _out_addr; 
+        console.log('# input file address : ' + this.in_addr);
+        console.log('# output file address : ' + this.out_addr);
         if(!fs.existsSync(this.out_addr) || !fs.existsSync(this.in_addr)) {
             console.log('error: input/out目录不存在');
             exit(-1); 
         } 
-        console.log('# input file address : ' + this.in_addr);
-        console.log('# output file address : ' + this.out_addr);
     }
 
     init() {
